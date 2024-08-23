75b1f8886c579372a253fa5591d6c9d92512d15e
Ihimbru-k <kanihims25@gmail.com>
2024-08-23 03:14:58 +0100

    initstate and dispose

--- lib/main.dart
+++ lib/main.dart
@@ -5,71 +5,135 @@ void main() {
   runApp(const MyApp());
 }
 
-
-
-
-class Contact{
-
+class Contact {
   final String name;
 
   Contact({required this.name});
-
 }
-
-
-
 
 // A singleton. Class which can be initialized only once
-
-class ContactBook{
+class ContactBook {
   ContactBook._sharedInstance();
 
-   static final ContactBook _shared = ContactBook._sharedInstance();
-   factory ContactBook( )=> _shared;
-   List<Contact> _contacts = [];
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
 
-   int get length => _contacts.length;
+  final List<Contact> _contacts = [];
 
+  int get length => _contacts.length;
 
-   void addContact({required Contact contact}) => _contacts.add(contact);
-   void removeContact({required Contact contact}) => _contacts.remove(contact);
+  void addContact({required Contact contact}) => _contacts.add(contact);
+  void removeContact({required Contact contact}) => _contacts.remove(contact);
 
-
-
-   Contact? contact({required int atIndex}) => _contacts.length > atIndex ? _contacts[atIndex] : null;
+  Contact? contact({required int atIndex}) =>
+      _contacts.length > atIndex ? _contacts[atIndex] : null;
 }
-
-
-
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final contactBook = ContactBook();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ListView.builder(itemBuilder: (context, index) {
-
-        final contact = contactBook.contact(atIndex: index);
-        ListTile(title : Text(contact!.name));
-
-
-        
-
-
-
-      }, itemCount: contactBook.length,)
+      home: const HomePage(),
     );
   }
 }
 
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final contactBook = ContactBook();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact List'),
+      ),
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index);
+          if (contact != null) {
+            return ListTile(
+              title: Text(contact.name),
+            );
+          } else {
+            return const SizedBox.shrink();
+          }
+        },
+      ),
+      floatingActionButton:  FloatingActionButton(onPressed: (){}, child: const Icon(Icons.add),),
+    );
+  }
+}
+
+
+
+class NewContactView extends StatefulWidget {
+  const NewContactView({super.key});
+
+  @override
+  State<NewContactView> createState() => _NewContactViewState();
+}
+
+class _NewContactViewState extends State<NewContactView> {
+
+  late final TextEditingController _controller;
+
+  @override
+  void setState(VoidCallback fn) {
+  _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: "Input contact name"
+            ),
+
+          ),
+          
+          TextButton(onPressed: (){
+            final contact = Contact(name: _controller.text);
+            ContactBook().addContact(contact: contact);
+
+            
+            
+            
+            
+          }, child: const Text("Add contact"),)
+
+
+
+        ],
+      ),
+
+
+
+
+
+
+    );
+  }
+}
 
